07d6c65773baf1b5806c528e5de9b5de8e40773e
g00405841 <g00405841@atu.ie>
2024-12-02 17:41:36 +0000

    Setup BackEnd + Database Connection

--- taskmanager/BackEnd/server.js
+++ taskmanager/BackEnd/server.js
@@ -19,10 +19,14 @@ app.use(function(req, res, next) {
   next();
 });
 
-// MongoDB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('MongoDB connection error:', err));
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err.message);
+  });
 
 // Task Model
 const taskSchema = new mongoose.Schema({
