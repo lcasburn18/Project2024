cf90f84ebf6005cb4334444acd653292569a657f
g00405841 <g00405841@atu.ie>
2024-12-12 15:47:45 +0000

    Working on Task Completion Date

--- taskmanager/src/components/TaskList.js
+++ taskmanager/src/components/TaskList.js
@@ -1,9 +1,22 @@
-import TaskItem from './TaskItem';
+import React from 'react';
 
-const TaskList = ({ tasks, reloadTasks }) => {
-  return tasks.map(task => (
-    <TaskItem key={task._id} task={task} reloadTasks={reloadTasks} />
-  ));
+const TaskList = ({ tasks, handleCompleteTask, setSelectedTaskId }) => {
+  return (
+    <div>
+      <h3>Pending Tasks</h3>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task._id}>
+            {task.title} - Due: {new Date(task.dueDate).toLocaleDateString()}
+            {/* Only show the 'Complete' button for tasks that are not completed */}
+            {!task.completed && (
+              <button onClick={() => setSelectedTaskId(task._id)}>Mark as Completed</button>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default TaskList;
