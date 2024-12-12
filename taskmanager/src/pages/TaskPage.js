52d84e9a1688603456e43316a0674a676cd6e92a
g00405841 <g00405841@atu.ie>
2024-12-12 21:52:27 +0000

    Removed TaskPage As It Was Unused

--- taskmanager/src/pages/TaskPage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-
-const TaskPage = () => {
-  const { id } = useParams();
-  return <h1>Task Details for ID: {id}</h1>;
-};
-
-export default TaskPage;
